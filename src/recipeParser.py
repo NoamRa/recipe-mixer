79275b95d5b976b7b098229e8a27f2f2935d912c
NoamRa <noamraby@gmail.com>
2019-06-05 21:35:41 +0300

    recipe parser - skeleton

---
+++ src/recipeParser.py
@@ -0,0 +1,44 @@
+"""
+
+ingredient shape
+{
+  {
+    quantity: number,
+    measure: string,
+    name: string,
+    liquidOrSolid: string
+  }
+}
+
+parsed_recipe shape
+{
+  ingredients: ingredient[],
+  instructions: string[]
+}
+
+
+"""
+
+
+def parse_ingredient():
+    ingredient = {}
+    return ingredient
+
+
+def parse_ingredients(recipe):
+    ingredients = []
+    return ingredients
+
+
+def parse_instructions(recipe):
+    instructions = []
+    return instructions
+
+
+def parse_recipe(recipe):
+    parsed_recipe = {}
+    parsed_recipe["ingredients"] = parse_ingredients(recipe)
+    parsed_recipe["instructions"] = parse_instructions(recipe)
+
+    return parsed_recipe
+
