d219b473b0914f22cc88b3aa9dac9519cbfa6c34
NoamRa <noamraby@gmail.com>
2019-06-08 10:09:39 +0300

    parse ingredients and instructions

--- src/logic/recipeParser.py
+++ src/logic/recipeParser.py
@@ -20,25 +20,48 @@ parsed_recipe shape
 """
 
 
-def parse_ingredient():
-    ingredient = {}
+def clean_recipe(recipe):
+    "break recipe text to list of lines, remove extra spaces and empty lines"
+    splited_recipe = recipe.splitlines()
+    cleaned = list(map(lambda x: x.strip(), splited_recipe))
+    cleaned = list(filter(lambda x: x != "", cleaned))
+    return cleaned
+
+
+def parse_ingredient(raw_ingrediant):
+    splitted = raw_ingrediant.split()
+    quantity = float(splitted[0])
+    measure = splitted[1]
+    name = " ".join(splitted[2:])
+
+    ingredient = {
+        "name": name,
+        "quantity": quantity,
+        "measure": measure,
+        "properties": [],
+    }
     return ingredient
 
 
-def parse_ingredients(recipe):
-    ingredients = []
+def parse_ingredients(raw_ingredients):
+    ingredients = list(map(lambda ingred: parse_ingredient(ingred), raw_ingredients))
     return ingredients
 
 
-def parse_instructions(recipe):
-    instructions = []
-    return instructions
+def parse_instructions(raw_instructions):
+    return raw_instructions
 
 
 def parse_recipe(recipe):
+    cleaned_recipe = clean_recipe(recipe)
+    ingredients_idx = cleaned_recipe.index("ingredients")
+    instructions_idx = cleaned_recipe.index("instructions")
+    ingredients_lines = cleaned_recipe[ingredients_idx + 1 : instructions_idx]
+    instructions_lines = cleaned_recipe[instructions_idx + 1 :]
+
     parsed_recipe = {
-        "ingredients": parse_ingredients(recipe),
-        "instructions": parse_instructions(recipe),
+        "ingredients": parse_ingredients(ingredients_lines),
+        "instructions": parse_instructions(instructions_lines),
     }
 
     return parsed_recipe
