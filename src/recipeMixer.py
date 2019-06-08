a5c495ac0e4d2877a477432da37b585f2fc94122
NoamRa <noamraby@gmail.com>
2019-06-08 18:51:23 +0300

    use texture_modifier

--- src/recipeMixer.py
+++ src/recipeMixer.py
@@ -9,10 +9,11 @@ import os
 # import flask
 
 # local imports
-from logic import recipeParser
+from logic import recipeParser, textureModifier
 
 
-config = {"recipeDir": ["..", "recipes", "cake.txt"]}
+config = {"recipeDir": ["..", "recipes", "cake.txt"], "do_random": False}
+do_random = config["do_random"]
 
 
 def main():
@@ -20,7 +21,16 @@ def main():
     with open(os.path.join(script_dir, *config.get("recipeDir")), "r") as reader:
         recipe = reader.read()
     parsed_recepie = recipeParser.parse_recipe(recipe)
-    print(json.dumps(parsed_recepie, indent=2))
+    # print(json.dumps(parsed_recepie, indent=2))
+
+    if do_random:
+        print("doing random!")
+    else:
+        print("not doing random")
+        transformed = textureModifier.texture_modifier(
+            parsed_recepie["ingredients"], 0.5
+        )
+        # print(json.dumps(transformed, indent=2))
 
 
 if __name__ == "__main__":
