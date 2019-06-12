a7582ac8e1a6d75a31ffffacd75b6b0a0092832f
NoamRa <noamraby@gmail.com>
2019-06-12 20:23:15 +0300

    arduino serial integration

--- src/recipeMixer.py
+++ src/recipeMixer.py
@@ -9,6 +9,7 @@ import logging
 
 # external packages
 from flask import Flask, render_template
+from flask_serial import Serial
 
 # local imports
 from logic import recipeParser, textureModifier, recipeFormatter, randomer
@@ -24,6 +25,27 @@ do_random = config["do_random"]
 script_dir = os.path.dirname(__file__)
 template_folder = os.path.join(script_dir, *config.get("templatesDir"))
 app = Flask(__name__, template_folder=template_folder)
+app.config["SERIAL_TIMEOUT"] = 0.1
+app.config["SERIAL_PORT"] = "COM2"
+app.config["SERIAL_BAUDRATE"] = 9600
+app.config["SERIAL_BYTESIZE"] = 8
+app.config["SERIAL_PARITY"] = "N"
+app.config["SERIAL_STOPBITS"] = 1
+
+ser = Serial(app)
+
+@ser.on_message()
+def handle_message(msg):
+    print("receive a message:", msg)
+    # send a msg of str
+    ser.on_send("send a str message!!!")
+    # send a msg of bytes
+    ser.on_send(b"send a bytes message!!!")
+
+
+@ser.on_log()
+def handle_logging(level, info):
+    print(level, info)
 
 
 def mix_recipe():
@@ -34,12 +56,12 @@ def mix_recipe():
 
     mixed_recipe = dict(parsed_recepie)
     if do_random:
-        print("doing random!")
+        # print("doing random!")
         mixed_recipe["ingredients"] = randomer.randomise_ingredients(
             parsed_recepie["ingredients"]
         )
     else:
-        print("not doing random")
+        # print("not doing random")
         mixed_recipe["ingredients"] = textureModifier.texture_modifier(
             parsed_recepie["ingredients"], 0.5
         )
@@ -64,5 +86,5 @@ def index():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080, debug=True)
+    app.run(host="0.0.0.0", port=80, debug=True)
 
