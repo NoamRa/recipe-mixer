e0562fb3dfd4c469327ef3c2ed77882a43f4ac74
NoamRa <noamraby@gmail.com>
2019-06-25 22:25:23 +0300

    add printer settings

--- src/config/config.py
+++ src/config/config.py
@@ -5,7 +5,7 @@ def getConf():
     return {
         # debug flags
         "debug": False,
-        "mock_serial": False,
+        "mock_serial": True,
         # server config
         "host": "0.0.0.0",
         "port": 9090,
@@ -17,5 +17,8 @@ def getConf():
         "serial_timeout": 0.01,
         # recipe dir
         "recipesDir": ["..", "recipes"],
+        # printer settings
+        "printer_name": "portable_printer",
+        "printer_opts": {}
     }
 
